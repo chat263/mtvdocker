0d4f9dc7c3fb015e2b3d33e17d45f4e621572d1a
chat263 <130237209+chat263@users.noreply.github.com>
2025-08-15 18:27:59 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // 浏览器端打包时忽略 Node 内置模块
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+      };
+    }
+    return config;
+  },
+};
+
+module.exports = nextConfig;
