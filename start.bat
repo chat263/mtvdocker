7889a5eb1cbc44b81dae4c18a87c18ec37914175
zhoufm <zhoufanmin@aliyun.com>
2025-08-19 11:32:37 +0800

    6

---
+++ start.bat
@@ -0,0 +1,9 @@
+set PORT=3000
+set PASSWORD=Cist123!@#
+set USERNAME=cistadmin
+set NEXT_PUBLIC_STORAGE_TYPE=upstash
+set REDIS_URL=redis://192.168.0.213:6379
+set UPSTASH_URL=https://integral-alien-15011.upstash.io
+set UPSTASH_TOKEN=ATqjAAIjcDFlZTdhYTkxMDhkYTU0ZDE0OTRmZWE5NTQ0MGVlYzY0ZnAxMA
+node start.js
+
